a50b49e34b1986b5f64c0d22c33513c9aa2354a5
Sergey <borovkovsergey364@gmail.com>
2020-03-30 14:36:39 +0300

    - tracked the route for user

--- PlacesApp/PlacesApp/MapViewController.swift
+++ PlacesApp/PlacesApp/MapViewController.swift
@@ -22,6 +22,7 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mapPin: UIImageView!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var goButton: UIButton!
     
     //MARK: - Properties
     
@@ -29,8 +30,22 @@ class MapViewController: UIViewController {
     var place = Place()
     let annotationIdentifier = "annotationIdentifier"
     let locationManager = CLLocationManager()
-    let regionInMeters = 10_000.00
+    let regionInMeters = 1_000.00
     var incomeSegueIdentifier = ""
+    
+    // saved directions array
+    
+    var directionsArray: [MKDirections] = []
+    
+    // get place coordinate to use it
+    var placeCoordinate: CLLocationCoordinate2D?
+    
+    // property for previous user's location
+    var previousLocation: CLLocation? {
+        didSet {
+            startTrackingUserLocation()
+        }
+    }
     
     //MARK: - Lifecycle
     
@@ -56,6 +71,11 @@ class MapViewController: UIViewController {
         //check actual user location
         showUserLocation()
     }
+    
+    @IBAction func goButtonTapped(_ sender: Any) {
+        getDirections()
+    }
+    
     @IBAction func doneButtonTapped(_ sender: Any) {
         
         guard let address = addressLabel.text, !address.isEmpty else { return }
@@ -69,12 +89,25 @@ class MapViewController: UIViewController {
     
     private func setupMapView() {
         
-        if incomeSegueIdentifier == "showDetailVC" {
+        goButton.isHidden = true
+        
+        if incomeSegueIdentifier == "showMapVC" {
             setupPlaceMark()
             mapPin.isHidden = true
             addressLabel.isHidden = true
             doneButton.isHidden = true
+            goButton.isHidden = false
         }
+    }
+    
+    private func resetMapView(with newDirections: MKDirections) {
+        
+        // we need to remove the old one to impose a new route
+        
+        mapView.removeOverlays(mapView.overlays)
+        directionsArray.append(newDirections)
+        let _ = directionsArray.map { $0.cancel() }
+        directionsArray.removeAll()
     }
     
     private func setupPlaceMark() {
@@ -113,6 +146,7 @@ class MapViewController: UIViewController {
             guard let placeMarkLocation = placeMark?.location else { return }
             
             annotation.coordinate = placeMarkLocation.coordinate
+            self.placeCoordinate = placeMarkLocation.coordinate
             
             // define a visible map area with all annotations created.
             
@@ -182,6 +216,35 @@ class MapViewController: UIViewController {
         }
     }
     
+    private func startTrackingUserLocation() {
+        
+        // update previous user location and show user in the center of the map view
+        // when user moves - update coordinates and show user in the center again
+        
+        guard let previousLocation = previousLocation else { return }
+        
+        let center = getCenterLocation(for: mapView)
+        
+        // if previous location more than 50 meters
+        
+        guard center.distance(from: previousLocation) > 50 else { return }
+        
+        // get new coordinates to previous location
+        
+        self.previousLocation = center
+        
+        // show updated user location for current route with delay 3 seconds
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) { [weak self] in
+            
+            guard let self = self else { return }
+            
+            self.showUserLocation()
+        }
+        
+        
+    }
+    
     private func getCenterLocation(for mapView: MKMapView) -> CLLocation {
         
         let latitude = mapView.centerCoordinate.latitude
@@ -198,6 +261,95 @@ class MapViewController: UIViewController {
         alertController.addAction(okAction)
         present(alertController, animated: true)
     }
+    
+    private func getDirections() {
+        
+        guard let location = locationManager.location?.coordinate else {
+            showAlert(title: "Error", message: "Current location is not found")
+            return }
+        
+        locationManager.startUpdatingLocation()
+        previousLocation = CLLocation(latitude: location.latitude, longitude: location.longitude)
+        
+        
+        guard let request = createDirectionsRequest(from: location) else {
+            showAlert(title: "Error", message: "Destination is not found")
+            return }
+        
+        // create a route based on what is available in the request.
+        
+        let directions = MKDirections(request: request)
+        
+        // reset routes before create a new one
+        
+        resetMapView(with: directions)
+        
+        directions.calculate { [weak self] (response, error) in
+            
+            guard let self = self else { return }
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard
+                let response = response else {
+                self.showAlert(title: "Error", message: "Directions is not available")
+                return }
+            
+            // the response object contains an array of routes
+            // these are possible directions for user
+            // each route object contains geometry information
+            // that can be used to show the route on the map
+            // as well as additional information such as expected travel time, distance and other route notifications
+            // using all this data, we can draw a route on the map.
+            
+            for route in response.routes {
+                
+                // add detailed geometry all the route
+                
+                self.mapView.addOverlay(route.polyline)
+                
+                // focus the map so that the whole route is visible.
+                
+                self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                
+                // distance and expected travel time
+                
+                let distance = String(format: "%.1f", route.distance / 1000)
+                let travelTime = route.expectedTravelTime
+                
+                print("Distance: \(distance)")
+                print("Expected travel time: \(travelTime)")
+            }
+        }
+    }
+    
+    private func createDirectionsRequest(from coordinate: CLLocationCoordinate2D) -> MKDirections.Request? {
+        
+        // get final point
+        
+        guard let destinationCoordinate = placeCoordinate else { return nil }
+        
+        // get starting point
+        let startingLocation = MKPlacemark(coordinate: coordinate)
+        
+        // get destination point
+        let destination = MKPlacemark(coordinate: destinationCoordinate)
+        
+        // create object which get the properties and will be return
+        // build a route
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: startingLocation)
+        request.destination = MKMapItem(placemark: destination)
+        request.transportType = .walking
+        request.requestsAlternateRoutes = true
+        
+        return request
+    }
+    
+    
 }
 
     //MARK: - Extension
@@ -235,6 +387,14 @@ extension MapViewController: MKMapViewDelegate {
         let center = getCenterLocation(for: mapView)
         let geoCoder = CLGeocoder()
         
+        if incomeSegueIdentifier == "getAddress" && previousLocation != nil {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                self.showUserLocation()
+            }
+        }
+        
+        geoCoder.cancelGeocode()
+        
         geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
             guard let self = self else { return }
             if let error = error {
@@ -261,6 +421,14 @@ extension MapViewController: MKMapViewDelegate {
                 }
             }
         }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        
+        let renderer = MKPolygonRenderer(overlay: overlay as! MKPolyline)
+        renderer.strokeColor = .blue
+        
+        return renderer
     }
 }
 
