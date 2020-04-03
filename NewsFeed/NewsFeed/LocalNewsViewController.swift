64ef71e9fe0da69671f53a5adfa4dfd967f8f628
Sergey <borovkovsergey364@gmail.com>
2020-04-03 13:29:57 +0300

    - initial commit - pod init

---
+++ NewsFeed/NewsFeed/LocalNewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LocalNewsViewController.swift
+//  NewsFeed
+//
+//  Created by SofiaBuslavskaya on 03/04/2020.
+//  Copyright © 2020 Sergey Borovkov. All rights reserved.
+//
+
+import UIKit
+
+class LocalNewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
